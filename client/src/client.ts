d1923fe763a13cd4823282b41ca5ec720b4ef6d5
ChenCMD <scnmed.g@gmail.com>
2022-11-11 14:18:45 +0900

    :construction: クライアント側を仮作成

--- client/src/client.ts
+++ client/src/client.ts
@@ -1,17 +1,18 @@
-import path from 'path'
-import * as vsc from 'vscode'
-import * as lc from 'vscode-languageclient/node.js'
+import * as vsc from 'vscode';
+import * as lc from 'vscode-languageclient/node.js';
 
-let client: lc.LanguageClient | undefined
+let client: lc.LanguageClient | undefined;
 
-export function activate(context: vsc.ExtensionContext) {
+export function activate({ extensionUri }: vsc.ExtensionContext): void {
+  vsc.window.showInformationMessage("client activated!!!!");
+
   // The server is implemented in node
-  const serverModule = vsc.Uri.joinPath(context.extensionUri, 'server', 'target', 'scala-3.2.1', 'impdocutil-fastopt', 'main.js').fsPath;
+  const serverModule = vsc.Uri.joinPath(extensionUri, 'server', 'dist', 'main.js').fsPath;
   // The debug options for the server
   // --inspect=6037: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
   const debugOptions = {
     execArgv: ['--nolazy', '--inspect=6037', '--expose-gc']
-  }
+  };
 
   // If the extension is launched in debug mode then the debug server options are used
   // Otherwise the run options are used
@@ -25,31 +26,45 @@ export function activate(context: vsc.ExtensionContext) {
       transport: lc.TransportKind.ipc,
       options: debugOptions,
     }
-  }
+  };
 
   const documentSelector: lc.DocumentSelector = [
     { language: 'mcfunction' }
-  ]
+  ];
 
   // Options to control the language client
   const clientOptions: lc.LanguageClientOptions = {
     documentSelector,
     initializationOptions: {},
     progressOnInitialization: true
-  }
+  };
 
   // Create the language client and start the client.
-  client = new lc.LanguageClient(
-    'IMPDocUtility',
-    'IMPDoc Utility',
-    serverOptions,
-    clientOptions
-  )
+  try {
+    client = new lc.LanguageClient(
+      'IMPDocUtility',
+      'IMPDoc Utility',
+      serverOptions,
+      clientOptions
+    );
+  } catch (e) {
+    console.error("e1:", e);
+  }
 
   // Start the client. This will also launch the server
-  client.start()
+  client?.start()
+    .then(() => console.log('server activated!!'))
+    .catch(e => console.error("e2:", e));
 }
 
-export function deactivate(): Thenable<void> | undefined {
-  return client?.stop()
+export async function deactivate(): Promise<void> {
+  return client?.needsStop() ? await client.stop() : undefined;
+}
+
+async function setTimeOut(millisecond: number): Promise<never> {
+  // eslint-disable-next-line brace-style
+  return await new Promise((_, reject) => setTimeout(
+    () => reject("timeout"),
+    millisecond
+  ));
 }
